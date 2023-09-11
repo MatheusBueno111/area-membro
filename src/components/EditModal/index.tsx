2d60eed664f6c8f6c16cc9f8b77da2e168a0b46b
Matheus Bueno <matheus.bueno.queiroz@gmail.com>
2023-09-11 17:01:36 -0300

    fix buttons

--- src/components/EditModal/index.tsx
+++ src/components/EditModal/index.tsx
@@ -1,5 +1,5 @@
 import { SandwichIcon } from '../Icons/SandwichIcon'
-import React, { useState } from 'react'
+import React from 'react'
 import * as S from './styles'
 import { Button } from '../CustomButtom'
 import ButtonControl from '../CustomButtom/ButtomControl'
@@ -10,43 +10,34 @@ interface EditModalProps {
 }
 
 const EditModal: React.FC<EditModalProps> = ({ onClose }) => {
-  const [isModalOpen, setIsModalOpen] = useState(true)
-  const handleCloseModal = () => {
-    setIsModalOpen(false)
-  }
-
   return (
-    <>
-      {isModalOpen && (
-        <S.Container>
-          <div className="container">
-            <div className="title">Ordenar carrocéis</div>
-            <div className="exit-icon" onClick={onClose}>
-              <ExitIcon />
-            </div>
-          </div>
-          <div className="container">
-            <SandwichIcon />
-            <p className="text">Carroussel1</p>
-            <div className="align-right">
-              <button className="edit-button">Editar</button>
-              <button className="exclude-button">Excluir</button>
-            </div>
-          </div>
-          <div className="container">
-            <SandwichIcon />
-            <p className="text">Carroussel2</p>
-            <div className="align-right">
-              <button className="edit-button">Editar</button>
-              <button className="exclude-button">Excluir</button>
-            </div>
-          </div>
-          <Button.Root bgcolor="#FFC019" className="login-button">
-            <ButtonControl type="submit">Salvar</ButtonControl>
-          </Button.Root>
-        </S.Container>
-      )}
-    </>
+    <S.Container>
+      <div className="container">
+        <div className="title">Ordenar carrocéis</div>
+        <div className="exit-icon" onClick={onClose}>
+          <ExitIcon />
+        </div>
+      </div>
+      <div className="container">
+        <SandwichIcon />
+        <p className="text">Carroussel1</p>
+        <div className="align-right">
+          <button className="edit-button">Editar</button>
+          <button className="exclude-button">Excluir</button>
+        </div>
+      </div>
+      <div className="container">
+        <SandwichIcon />
+        <p className="text">Carroussel2</p>
+        <div className="align-right">
+          <button className="edit-button">Editar</button>
+          <button className="exclude-button">Excluir</button>
+        </div>
+      </div>
+      <Button.Root bgcolor="#FFC019" className="login-button">
+        <ButtonControl type="submit">Salvar</ButtonControl>
+      </Button.Root>
+    </S.Container>
   )
 }
 
--- src/components/NewCarrousselModal/index.tsx
+++ src/components/NewCarrousselModal/index.tsx
@@ -11,11 +11,6 @@ interface NewCarrousselModalProps {
 
 const NewCarrousselModal: React.FC<NewCarrousselModalProps> = ({ onClose }) => {
   const [carouselName, setCarouselName] = useState('')
-  const [isModalOpen, setIsModalOpen] = useState(true)
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false)
-  }
 
   const handleCarouselNameChange = (
     event: React.ChangeEvent<HTMLInputElement>,
@@ -24,32 +19,28 @@ const NewCarrousselModal: React.FC<NewCarrousselModalProps> = ({ onClose }) => {
   }
 
   return (
-    <>
-      {isModalOpen ? (
-        <S.Container>
-          <div className="container">
-            <div className="title">Novo Carrossel</div>
-            <div className="exit-icon" onClick={onClose}>
-              <ExitIcon />
-            </div>
-          </div>
-          <Input.Root className="input-container">
-            <Input.Control
-              type="text"
-              placeholder="Nome do carrossel"
-              className={`carroussel-input ${carouselName ? 'filled' : ''}`}
-              value={carouselName}
-              onChange={handleCarouselNameChange}
-            />
-          </Input.Root>
-          <Button.Root
-            className={`salvar-button ${carouselName ? 'yellow' : 'gray'}`}
-          >
-            <ButtonControl type="submit">Salvar</ButtonControl>
-          </Button.Root>
-        </S.Container>
-      ) : null}
-    </>
+    <S.Container>
+      <div className="container">
+        <div className="title">Novo Carrossel</div>
+        <div className="exit-icon" onClick={onClose}>
+          <ExitIcon />
+        </div>
+      </div>
+      <Input.Root className="input-container">
+        <Input.Control
+          type="text"
+          placeholder="Nome do carrossel"
+          className={`carroussel-input ${carouselName ? 'filled' : ''}`}
+          value={carouselName}
+          onChange={handleCarouselNameChange}
+        />
+      </Input.Root>
+      <Button.Root
+        className={`salvar-button ${carouselName ? 'yellow' : 'gray'}`}
+      >
+        <ButtonControl type="submit">Salvar</ButtonControl>
+      </Button.Root>
+    </S.Container>
   )
 }
 
